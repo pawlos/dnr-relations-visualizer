6c4cdd172d89133a02f60e43f76477de817dddd2
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2014-07-13 16:31:19 +0200

    Parse episode logic extracted to separate method

--- parser.py
+++ parser.py
@@ -1,6 +1,9 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+def parseEpisode(episode):
+	return	{ 'no': int(episode.font.text), 
+			  'title': episode.a.text }
 
 url = 'http://www.dotnetrocks.com/archives.aspx'
 
@@ -9,9 +12,7 @@ episodes = urllib2.urlopen(url)
 html = episodes.read()
 parsed_html = BeautifulSoup(html)
 
-items = map(lambda episode: 
-				{ 'no': int(episode.font.text), 
-				  'title': episode.a.text }, 
+items = map(parseEpisode, 
 			parsed_html.findAll('tr', attrs={'class':'archivecell'}))
 sortedEposides = sorted(items, key=lambda item: item.__getitem__, reverse=True)
 print sortedEposides
